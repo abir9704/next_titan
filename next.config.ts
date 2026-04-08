81b8b71a1fa22f60d4304ec9c91018a3ca67267d
abir781 <hasan.abir3176@gmail.com>
2026-04-08 12:47:56 +0600

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
+   images: {
+    domains: ['i.ibb.co.com'], // এই host add করতে হবে
+  },
   /* config options here */
 };
 
