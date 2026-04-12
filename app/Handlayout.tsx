0617d94c0df7ec66eca18e36155e9a4d6a59d250
abir781 <hasan.abir3176@gmail.com>
2026-04-12 12:54:29 +0600

    kkkksss

--- app/Handlayout.tsx
+++ app/Handlayout.tsx
@@ -43,24 +43,44 @@ const Handlayout = () => {
   <div className="absolute inset-0 bg-[url('/rt567.jpg')] bg-cover bg-center opacity-30 mix-blend-overlay"></div>
 
   {/* Hexagon */}
- <div className="absolute top-[120px] right-[120px] h-[500px] rotate-[17deg]">
+<div className="absolute top-[120px] right-[120px] h-[500px] ">
   <svg width="350" height="350" viewBox="0 0 100 100">
 
     <defs>
+      {/* gradient */}
       <linearGradient id="silverFade" x1="0%" y1="0%" x2="100%" y2="100%">
         <stop offset="0%" stopColor="#0a0a0a" />
         <stop offset="30%" stopColor="#bfbfbf" />
         <stop offset="70%" stopColor="#f5f5f5" />
         <stop offset="100%" stopColor="#1a1a1a" />
       </linearGradient>
+
+      {/* clip path */}
+      <clipPath id="hexClip">
+        <polygon points="25,5 75,5 95,50 75,95 25,95 5,50" />
+      </clipPath>
     </defs>
 
-    <polygon
-      points="25,5 75,5 95,50 75,95 25,95 5,50"
-      fill="none"
-      stroke="url(#silverFade)"
-      strokeWidth="2.5"
-    />
+    {/* IMAGE (overflow feel with bigger size) */}
+   <image
+  href="/r809.jpg"
+  x="0"
+  y="0"
+  width="100"
+  height="100"
+  preserveAspectRatio="xMidYMid slice"
+  clipPath="url(#hexClip)"
+/>
+
+    {/* BORDER */}
+ <g transform="rotate(17 50 50)">
+  <polygon
+    points="25,5 75,5 95,50 75,95 25,95 5,50"
+    fill="none"
+    stroke="url(#silverFade)"
+    strokeWidth="2.5"
+  />
+</g>
 
   </svg>
 </div>
